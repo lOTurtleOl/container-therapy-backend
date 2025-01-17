b5bb78aa324490e0462c3fc1d29ad92f5f46ea36
Andrew Hughes <andyashmore1@gmailcom>
2025-01-16 19:48:21 -0600

    add springboot dependencies

---
+++ src/main/java/container/therapy/ContainerTherapyApplication.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package container.therapy;
+
+/**
+ * 
+ */
+public class ContainerTherapyApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
